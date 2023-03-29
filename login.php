ee438d200b226b20c4bc74049e7bc2a42f7c404b
certifiersidi <98776450+certifiersidi@users.noreply.github.com>
2023-03-29 15:54:57 +0100

    Add files via upload

---
+++ login.php
@@ -0,0 +1,28 @@
+<?php 
+  include_once 'header.php';
+?>
+
+<section class="signup-form">
+    <h2>Log in</h2>
+    <form action="login.inc.php" method="post">
+        <input type="text" name="uid" placeholder="username">
+        <input type="password" name="pwd" placeholder="password">
+        <button type="submit" name="submit">Log in</button>
+    </form>
+    <?php
+      if (isset($_GET["error"])){
+        if($_GET["error"] == "emptyinput"){
+          echo "Fill in all the fields";
+        }
+        else if($_GET["error"] == "wronglogin"){
+          echo "There is an incorrect information";
+        }
+      }
+    ?>
+</section>
+  <script type="text/javascript" src="script.js"></script>
+  </body>
+</html>
+<?php 
+  include_once 'footer.php';
+?>
